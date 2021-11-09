bb60ce2eddb9d9a9ad51397854135e5d14519360
Kat <kyevsukov@gmail.com>
2021-11-09 17:08:50 -0500

    imported token secret into builder

--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const { TOKEN_SECRET } = require('../../config')
 
 module.exports = function buildToken(user){
     const payload = {
@@ -9,5 +10,5 @@ module.exports = function buildToken(user){
     const options = {
         expiresIn: '1d',
     }
-    return jwt.sign(payload, 'shhh', options)
+    return jwt.sign(payload, TOKEN_SECRET, options)
 }
